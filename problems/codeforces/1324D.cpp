4e254a0ca9988eeea98fc8a502dde33ad6efa3c9
Dinh Minh Vu <dminhvu.work@gmail.com>
2025-12-22 22:56:19 +0100

    add: implement solution for Codeforces problem 1324D, create new file for Codeforces problem 1526B, and update template structure

--- problems/codeforces/1324D.cpp
+++ problems/codeforces/1324D.cpp
@@ -1,27 +1,146 @@
-#include<bits/stdc++.h>
+/*
+    Task: Codeforces 1324D
+    Date: 22.12.2025
+    Time: 21:55:03 CET
+    Author: https://www.linkedin.com/in/dminhvu02
+*/
 
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
 using namespace std;
-const int N = 2e5 + 5;
 
-int n;
-vector<int> a, b;
-map<int, int> bit;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define per(i, a, b) for (int i = (b); i >= (a); --i)
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const ld EPS = 1e-9;
+const int MOD = 998244353;
+
+void fastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void solve() {
+    /* Solution goes here */
+    /*
+5
+4 8 2 6 2
+4 5 4 1 3
+
+a[i] + a[j] > b[i] + b[j]
+
+O(n^2) -> TLE due to n <= 2*10^5
+for i = 0..n
+    for j = 0..i - 1
+        if a[i] + a[j] > b[i] + b[j]
+            count += 1
+
+# sample input 1
+# idea 1: sort by a[i]
+8 6 4 2 2
+5 1 4 3 4
+2 4 1 5 3
+
+# idea 2: sort by a[i] - b[i]
+a[i] - b[i] > b[j] - a[j]
+a[i] - b[i] > -(a[j] - b[j])
+
+a[i] - b[i]: 0 3 -2 5 -1
+index:       0 1  2 3  4
+
+sorted:
+-2 -1 0 3 5
+2   4 0 1 3
+
+2 + 2 + 2 + 1
+
+-> index does not matter
+
+# sample input 2
+1 3 2 4
+1 3 2 4
+
+0 0 0 0
+    */
+
+    int n;
+    cin >> n;
+    vpii a(n + 1);
+    rep(i, 1, n) {
+        int v;
+        cin >> v;
+        a[i].first = v;
+    }
+    rep(i, 1, n) {
+        int v;
+        cin >> v;
+        a[i].second = v;
+    }
+
+    sort(a.begin() + 1, a.end(), [](pii a, pii b) {
+        return (a.first - a.second) < (b.first - b.second);
+    });
+
+    // rep(i, 1, n) { cout << a[i].first << " " << a[i].second << "\n"; }
+
+    ll ans = 0;
+    rep(i, 1, n) {
+        auto it =
+            upper_bound(a.begin() + i + 1, a.end(), -(a[i].first - a[i].second),
+                        [](int value, const pii& p) {
+                            return value < (p.first - p.second);
+                        });
+
+        if (it != a.end()) {
+            int j = it - a.begin();
+            // cout << i << " " << -(a[i].first - a[i].second) << " " << j <<
+            // "\n";
+            ans += (n - j + 1);
+        }
+    }
+
+    cout << ans;
+}
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    freopen("input.txt","r",stdin);
-    cin >> n;
-    a.resize(n + 1);
-    b.resize(n + 1);
-    for (int i = 1; i <= n; i++) cin >> a[i];
-    for (int i = 1; i <= n; i++) cin >> b[i];
-    multiset<int> ms;
-    int64_t ans = 0;
-    for (int i = n; i >= 1; i--) {
-        ans += calc(a[i] - b[i] - 1);
-        ans += 1ll * (k - ms.begin());
-        ms.insert(b[i] - a[i]);
+    fastIO();
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
     }
-    cout << ans << '\n';
+
     return 0;
 }
--- problems/codeforces/1526B.cpp
+++ problems/codeforces/1526B.cpp
@@ -1,3 +1,10 @@
+/*
+    Task: Codeforces 1526B
+    Date: 22.12.2025
+    Time: 19:46:34 CET
+    Author: https://www.linkedin.com/in/dminhvu02
+*/
+
 #include <algorithm>
 #include <cmath>
 #include <cstring>
@@ -12,6 +19,7 @@
 #include <vector>
 using namespace std;
 
+// Type aliases
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
@@ -21,6 +29,7 @@ using vi = vector<int>;
 using vll = vector<ll>;
 using vpii = vector<pii>;
 
+// Macros
 #define pb push_back
 #define mp make_pair
 #define fi first
@@ -29,29 +38,50 @@ using vpii = vector<pii>;
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) (int)(x).size()
 
+// 1-indexed loops
 #define rep(i, a, b) for (int i = (a); i <= (b); ++i)
 #define per(i, a, b) for (int i = (b); i >= (a); --i)
 
+// Constants
 const int INF = 1e9 + 7;
 const ll LINF = 1e18 + 7;
 const ld EPS = 1e-9;
 const int MOD = 998244353;
 
+// Fast I/O
 void fastIO() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 }
 
+// Solution function
 void solve() {
-    /* Solution goes here */
+    // x = 11a + 111b + 1111c + ... + 111111111h
+    // 1111 = 11*100 + 11
+    // 11111 = 111*100 + 11
+    // 111111 = 111*1000 + 111
+    // -> x = 11x + 111y
+
+    // 144 = 100 * 1 + 11 * 4
+    int x;
+    cin >> x;
+
+    bool yes = false;
+    if (x < 111) {
+        yes = x % 11 == 0;
+    } else {
+        yes = x % 11 == 0 || x >= 111 * (x % 11);
+    }
+
+    cout << (yes ? "YES" : "NO") << "\n";
 }
 
 int main() {
     fastIO();
 
     int t = 1;
-    cin >> t;
+    cin >> t;  // Comment out if single test case
 
     while (t--) {
         solve();
