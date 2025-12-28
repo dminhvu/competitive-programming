9e2b5675c09ff8e157d14543f2d356c001e2a2a7
Dinh Minh Vu <dminhvu.work@gmail.com>
2025-12-28 20:26:38 +0100

    add: atcoder: ABV438B

---
+++ problems/atcoder/ABC438B.cpp
@@ -0,0 +1,113 @@
+/*
+    Task: AtCoder ABC438B
+    Date: 28.12.2025
+    Time: 20:11:28 CET
+    Author: https://www.linkedin.com/in/dminhvu02
+*/
+
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) \
+    cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#else
+#define debug(x)
+#define debug2(x, y)
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define per(i, a, b) for (int i = (b); i >= (a); --i)
+
+const int INF = INT_MAX;
+const int NINF = INT_MIN;
+const ll LINF = LLONG_MAX;
+const ll NLINF = LLONG_MIN;
+const ld EPS = 1e-9;
+const int MOD = 998244353;
+
+void setup() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+#ifdef LOCAL
+    if (!freopen("input.txt", "r", stdin)) {
+        cerr << "Warning: Could not open input.txt" << endl;
+    }
+    if (!freopen("output.txt", "w", stdout)) {
+        cerr << "Warning: Could not open output.txt for writing" << endl;
+    }
+#endif
+}
+
+void solve() {
+    /* SOLUTION GOES HERE */
+    /* ================== */
+    int n, m;
+    cin >> n >> m;
+    string s, t;
+    cin >> s >> t;
+
+    int best = INF;
+    rep(i, 0, n - m) {
+        int cost = 0;
+        rep(j, 0, m - 1) {
+            int x = (int)(s[i + j] - '0');
+            int y = (int)(t[j] - '0');
+            debug2(x, y);
+
+            if (x < y) {
+                x += 10;
+            }
+            cost += x - y;
+        }
+        debug(cost);
+        best = min(best, cost);
+    }
+
+    cout << best << '\n';
+}
+
+int main() {
+    setup();
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
