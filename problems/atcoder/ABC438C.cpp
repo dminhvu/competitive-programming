b2c9814b539f54673223965ae6e6502b7b5c4f86
Dinh Minh Vu <dminhvu.work@gmail.com>
2025-12-28 21:05:59 +0100

    add: atcoder: ABC438C

---
+++ problems/atcoder/ABC438C.cpp
@@ -0,0 +1,106 @@
+/*
+    Task: AtCoder ABC438C
+    Date: 28.12.2025
+    Time: 20:11:31 CET
+    Author: https://www.linkedin.com/in/dminhvu02
+*/
+
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) \
+    cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#else
+#define debug(x)
+#define debug2(x, y)
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define per(i, a, b) for (int i = (b); i >= (a); --i)
+
+const int INF = INT_MAX;
+const int NINF = INT_MIN;
+const ll LINF = LLONG_MAX;
+const ll NLINF = LLONG_MIN;
+const ld EPS = 1e-9;
+const int MOD = 998244353;
+
+void setup() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+#ifdef LOCAL
+    if (!freopen("input.txt", "r", stdin)) {
+        cerr << "Warning: Could not open input.txt" << endl;
+    }
+    if (!freopen("output.txt", "w", stdout)) {
+        cerr << "Warning: Could not open output.txt for writing" << endl;
+    }
+#endif
+}
+
+void solve() {
+    /* SOLUTION GOES HERE */
+    /* ================== */
+    int n;
+    cin >> n;
+    vi a(n);
+    rep(i, 0, n - 1) cin >> a[i];
+    vi st;
+
+    rep(i, 0, n - 1) {
+        st.pb(a[i]);
+        while (st.size() >= 4 && st[st.size() - 4] == st[st.size() - 3] &&
+               st[st.size() - 3] == st[st.size() - 2] &&
+               st[st.size() - 2] == st[st.size() - 1]) {
+            rep(j, 1, 4) { st.pop_back(); }
+        }
+    }
+
+    cout << st.size() << '\n';
+}
+
+int main() {
+    setup();
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
