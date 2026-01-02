f0683e79e940fb6940d76b7a037bc4132a677102
Dinh Minh Vu <dminhvu.work@gmail.com>
2026-01-02 17:23:05 +0100

    feat: add CSES 1636 and CSES 1637 problem solutions

---
+++ problems/cses/1636.cpp
@@ -0,0 +1,141 @@
+/*
+    Task: CSES 1635
+    Date: 01.01.2026
+    Time: 23:44:00 CET
+    Author: https://www.linkedin.com/in/dminhvu02
+*/
+
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) \
+    cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#else
+#define debug(x)
+#define debug2(x, y)
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define per(i, a, b) for (int i = (b); i >= (a); --i)
+
+const int INF = 1e9 + 210903;
+const ll LINF = 1e18 + 210903;
+const ld EPS = 1e-9;
+const ll MOD = 1e9 + 7;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+bool chmin(T& a, const T& b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+ll binpow(ll a, ll b, ll mod = LINF) {
+    ll res = 1;
+    a %= mod;
+    while (b > 0) {
+        if (b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+void setup() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+#ifdef LOCAL
+    if (!freopen("input.txt", "r", stdin)) {
+        cerr << "Warning: Could not open input.txt" << endl;
+    }
+    if (!freopen("output.txt", "w", stdout)) {
+        cerr << "Warning: Could not open output.txt for writing" << endl;
+    }
+#endif
+}
+
+void solve() {
+    /* SOLUTION GOES HERE */
+    /* ================== */
+    /*
+TODO: Rethink
+    */
+    int n, x;
+    cin >> n >> x;
+    int c[n];
+    rep(i, 0, n - 1) cin >> c[i];
+    sort(c, c + n);
+    int dp[x + 1];
+    memset(dp, 0, sizeof(dp));
+    dp[0] = 1;
+
+    rep(j, 0, n - 1) {
+        rep(i, c[j], x) {
+            // dp[i] = 0;
+            if (i - c[j] >= 0 && dp[i - c[j]]) {
+                dp[i] += dp[i - c[j]];
+                dp[i] %= MOD;
+            }
+        }
+    }
+
+    cout << dp[x] << '\n';
+}
+
+int main() {
+    setup();
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
---
+++ problems/cses/1637.cpp
@@ -0,0 +1,159 @@
+/*
+    Task: CSES 1637
+    Date: 02.01.2026
+    Time: 17:08:53 CET
+    Author: https://www.linkedin.com/in/dminhvu02
+*/
+
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) \
+    cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#else
+#define debug(x)
+#define debug2(x, y)
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define per(i, a, b) for (int i = (b); i >= (a); --i)
+
+const int INF = 1e9 + 210903;
+const ll LINF = 1e18 + 210903;
+const ld EPS = 1e-9;
+const ll MOD = 998244353;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+bool chmin(T& a, const T& b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+ll binpow(ll a, ll b, ll mod = LINF) {
+    ll res = 1;
+    a %= mod;
+    while (b > 0) {
+        if (b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+void setup() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+#ifdef LOCAL
+    if (!freopen("input.txt", "r", stdin)) {
+        cerr << "Warning: Could not open input.txt" << endl;
+    }
+    if (!freopen("output.txt", "w", stdout)) {
+        cerr << "Warning: Could not open output.txt for writing" << endl;
+    }
+#endif
+}
+
+void solve() {
+    /* SOLUTION GOES HERE */
+    /* ================== */
+    /*
+
+10: how to make 10 from x?
+x can be: 10, 12, 13, ..., 19
+
+11: how to make 11 from x?
+x can be: 12
+
+18: how to make 18 from x?
+x can be: 20, 19
+
+-> let's think in reverse:
+what numbers can x make:
+
+20: can make 18
+19: can make: 18, 10
+18: can make: 17, 10
+and so on
+    */
+    int n;
+    cin >> n;
+    int dp[n + 1];
+    rep(i, 0, n - 1) dp[i] = INF;
+    dp[n] = 0;
+
+    per(i, 0, n) {
+        int num = i;
+        vi digits;
+        while (num) {
+            digits.pb(num % 10);
+            num /= 10;
+        }
+
+        for (auto d : digits) {
+            if (i - d >= 0) {
+                chmin(dp[i - d], dp[i] + 1);
+            }
+        }
+    }
+
+    cout << dp[0] << '\n';
+}
+
+int main() {
+    setup();
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
