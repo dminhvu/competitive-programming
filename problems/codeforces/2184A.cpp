dd924d2313d61c10cedcf1189a99061adba29d50
Dinh Minh Vu <dminhvu.work@gmail.com>
2026-01-12 20:08:50 +0100

    feat: add solutions for Codeforces 2184A, 2184B, and 2184C problems

---
+++ problems/codeforces/2184A.cpp
@@ -0,0 +1,143 @@
+/*
+    Task: Codeforces 2184A
+    Date: 12.01.2026
+    Time: 15:28:22 CET
+    Author: https://www.linkedin.com/in/dminhvu02
+*/
+
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) \
+    cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#else
+#define debug(x)
+#define debug2(x, y)
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define per(i, a, b) for (int i = (b); i >= (a); --i)
+
+const int INF = 1e9 + 210903;
+const ll LINF = 1e18 + 210903;
+const ld EPS = 1e-9;
+const ll MOD = 998244353;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+bool chmin(T& a, const T& b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+ll binpow(ll a, ll b, ll mod = LINF) {
+    ll res = 1;
+    a %= mod;
+    while (b > 0) {
+        if (b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+void setup() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+#ifdef LOCAL
+    if (!freopen("input.txt", "r", stdin)) {
+        cerr << "Warning: Could not open input.txt" << endl;
+    }
+    if (!freopen("output.txt", "w", stdout)) {
+        cerr << "Warning: Could not open output.txt for writing" << endl;
+    }
+#endif
+}
+
+void solve() {
+    /* SOLUTION GOES HERE */
+    /* ================== */
+    /*
+
+4: 2 2 -> 0
+5: 2 3 -> 1
+6: 3 3 -> 0
+7: 2 2 3 -> 1
+8: 2 2 2 2 -> 0
+9: 2 2 2 3 -> 1
+10: 2 3 2 3 -> 0
+11: 3 3 2 3 -> 1
+12: 3 3 3 3 -> 0
+
+
+    */
+    int n;
+    cin >> n;
+    if (n == 2) {
+        cout << 2 << '\n';
+    } else if (n == 3) {
+        cout << 3 << '\n';
+    } else if (n % 2 != 0) {
+        cout << 1 << '\n';
+    } else {
+        cout << 0 << '\n';
+    }
+}
+
+int main() {
+    setup();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
---
+++ problems/codeforces/2184B.cpp
@@ -0,0 +1,179 @@
+/*
+    Task: Codeforces 2184B
+    Date: 12.01.2026
+    Time: 15:28:22 CET
+    Author: https://www.linkedin.com/in/dminhvu02
+*/
+
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << (x) << endl
+#define debug2(x, y) \
+    cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#else
+#define debug(x)
+#define debug2(x, y)
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define per(i, a, b) for (int i = (b); i >= (a); --i)
+
+const int INF = 1e9 + 210903;
+const ll LINF = 1e18 + 210903;
+const ld EPS = 1e-9;
+const ll MOD = 998244353;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+bool chmin(T& a, const T& b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+ll binpow(ll a, ll b, ll mod = LINF) {
+    ll res = 1;
+    a %= mod;
+    while (b > 0) {
+        if (b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+void setup() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+#ifdef LOCAL
+    if (!freopen("input.txt", "r", stdin)) {
+        cerr << "Warning: Could not open input.txt" << endl;
+    }
+    if (!freopen("output.txt", "w", stdout)) {
+        cerr << "Warning: Could not open output.txt for writing" << endl;
+    }
+#endif
+}
+
+void solve() {
+    /* SOLUTION GOES HERE */
+    /* ================== */
+    /*
+
+hourglass
+s minutes
+flip every k minutes
+leave in m minutes
+
+10
+k <= s
+x >= m
+
+0 ---- s
+0 ---- k
+
+(m / k) + (m % k)
+
+
+min(s, (m / k + 1) * k - m)
+
+(m / k) * k + s - m
+
++ s if s <= k
++ k if k < s
+
+m -> inf no more flip
+
+how many minutes
+
+last time of flip = t
+how many minutes will continue to flip
+
+t = m / k
+if t % 2 == 0
+    min(s, k)
+
+3 1 5
+
+
+
+3 6 12
+3
+
+8 3 15
+5-3 0 +3
+8-0 1 +8
+5-3 2
+8-0 3
+5-3 4
+
+4 6 12
+
+
+    */
+    ll s, k, m;
+    cin >> s >> k >> m;
+    ll t = (m / k);
+    ll ans = t * k;
+    ans += ((s <= k || t % 2 == 0) ? s : k);
+    ans -= m;
+    chmax(ans, 0ll);
+    cout << ans << '\n';
+}
+
+int main() {
+    setup();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
